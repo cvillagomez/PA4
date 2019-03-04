70dd379855d6a8f78208eb6505f837c832197bc0
Carlos Villagomez <villagoc@gmail.com>
2019-03-03 18:32:40 -0800

    Initial commit...

---
+++ pa4.py
@@ -0,0 +1,10 @@
+# Programmer: Carlos Villagomez
+# Class: CPSC 310-01, Spring 2019
+# Programming Assignment #4
+# 3/3/19
+#
+# Description: This program builds simple classifiers for the
+# "pre-processed" automobile dataset that was created for PA1
+# by implementing a linear regression classifier, kNN classifier, and 
+# evaluates classifiers using training/test sets sampled by using random
+# sub-sampling and stratified k-fold cross validation
